6164cae09d279b3adae73a6e3c8aaccb3a4a467e
greysou1 <prudvikamtam@knights.ucf.edu>
2023-10-09 19:22:45 -0400

    updated merge_sils: reads indices from pkl file

--- misc/merge_sils.py
+++ misc/merge_sils.py
@@ -1,12 +1,17 @@
-import os
+import os, pickle
 import cv2
 from tqdm import tqdm
 import natsort
 import threading
 
 def convert_folder_to_video(folder_path):
-    # Get the list of PNG files in the folder
+    filename = folder_path.replace(main_folder, "").split("/")
+    filename = [a for a in filename if a != ""]
+    filename = "-".join(filename)
+    video_indices = INDICES[filename]
+
     image_files = [file for file in os.listdir(folder_path) if file.lower().endswith('.png')]
+
     # image_files = sorted(image_files, key=lambda x: int(x.split('-')[-1].split('.')[0]))
     image_files = natsort.natsorted(image_files)
 
@@ -14,26 +19,25 @@ def convert_folder_to_video(folder_path):
         # print(f"No PNG files found in {folder_path}")
         return
 
-    # Read the first image to get the size
     first_image = cv2.imread(os.path.join(folder_path, image_files[0]))
     height, width, _ = first_image.shape
 
     video_file = f"{folder_path}.mp4"
 
     if os.path.exists(video_file):
-        pbar.set_description(f'{video_file} exists!')
-        pbar.refresh()
-        pbar.update(1)
+        # pbar.set_description(f'{video_file} exists!')
+        # pbar.refresh()
+        # pbar.update(1)
         return
 
-    # print(folder_path,video_file)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video_writer = cv2.VideoWriter(video_file, fourcc, 30.0, (width, height))
 
     for image_file in image_files:
-        image_path = os.path.join(folder_path, image_file)
-        image = cv2.imread(image_path)
-        video_writer.write(image)
+        if int(image_file.split(".")[0].split("-")[4]) in video_indices:
+            image_path = os.path.join(folder_path, image_file)
+            image = cv2.imread(image_path)
+            video_writer.write(image)
 
     video_writer.release()
     # print(f"Conversion completed for folder: {folder_path}")
@@ -51,11 +55,19 @@ def process_folders(folder_path):
     for thread in threads:
         thread.join()
 
+def load_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        pickle_data = pickle.load(file)
+    return pickle_data
+
+main_folder = "/home/prudvik/id-dataset/Grounded-Segment-Anything/outputs/silhouettes-shirts/"
+pickle_file = '/home/c3-0/datasets/ID-Dataset/casiab/metadata/casiab_indices.pkl'
+INDICES = load_pickle(pickle_file) # loads a dictionary
+
 if __name__ == "__main__":
     num_threads = os.sysconf(os.sysconf_names['SC_NPROCESSORS_ONLN'])
     print("Number of threads available:", num_threads)
-
-    main_folder = "/home/prudvik/id-dataset/Grounded-Segment-Anything/outputs/silhouettes-shirts"
+    
     ids = natsort.natsorted(os.listdir(main_folder))
 
     start_id, end_id = 0, 20
@@ -80,3 +92,14 @@ if __name__ == "__main__":
                 pbar.set_description(f'shirts-{id}-{cond}')
                 pbar.refresh()
                 pbar.update(1)
+
+# casiab_indices
+# >>>import pickle
+# >>>with open('/home/c3-0/datasets/ID-Dataset/casiab/metadata/casiab_indices.pkl', 'rb') as file:
+# ...     loaded_data = pickle.load(file)
+# ...
+# >>>loaded_data["003-nm-02-054"]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ... , 114, 115]
+#
+# pickle file created using script
+# https://github.com/greysou1/augset/blob/main/misc/merge_indices.py
